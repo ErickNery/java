0916d325dd8c500c9bff5c7cd868419bd6440918
Erick Nery <erick.a.a.nery@gmail.com>
2019-03-21 21:52:19 -0300

    subindo projeto soap springboot

---
+++ springboot-soap-bookstore/src/main/java/com/github/springbootsoapbookstore/SpringbootSoapBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.springbootsoapbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapBookstoreApplication.class, args);
+	}
+
+}
